3bfe1f30b324f48183041154f9361484045b44fb
venkatesh.orugunta <venkatesh.orugunta@dyedurham.com>
2025-09-23 20:03:35 -0400

    Update Next.js configuration and GitHub Actions workflows: refactor next.config.js for base path support, create new publish workflow for GitHub Pages deployment, and add setup-node action for streamlined Node.js environment setup.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-module.exports = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  experimental: {
-    appDir: true,
-  },
-}
+// module.exports = {
+//   output: "export",
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true
+//   },
+//   experimental: {
+//     appDir: true,
+//   },
+// }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/robert-portfolio",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
